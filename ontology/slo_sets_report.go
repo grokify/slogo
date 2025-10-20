94d210e056af4da4f08ec5654269f54b79659414
John Wang <johncwang@gmail.com>
2025-10-19 17:58:29 -0700

    feat: `ontology`: add `WriteSLOSetsReport()`

---
+++ ontology/slo_sets_report.go
@@ -0,0 +1,71 @@
+package ontology
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	v1 "github.com/OpenSLO/go-sdk/pkg/openslo/v1"
+	"github.com/grokify/gocharts/v2/data/histogram"
+	"github.com/grokify/mogo/type/maputil"
+)
+
+func WriteSLOSetsReport(w io.Writer, slos map[string][]v1.SLO) error {
+	// Get label definitions
+	labelDefs := GetLabelDefinitions()
+
+	// Write header
+	fmt.Fprintln(w, "# SLO Metrics by Ontology Labels")
+	fmt.Fprintln(w)
+	fmt.Fprintln(w, "This document shows the distribution of SLOs across different ontology label dimensions.")
+	fmt.Fprintln(w)
+
+	hsetsLabelSetValue := histogram.NewHistogramSets("")
+
+	// Get sorted label names
+	labelOrder := Labels()
+	// For each label type, create a table
+	for _, labelName := range labelOrder {
+		_, exists := labelDefs[labelName]
+		if !exists {
+			continue
+		}
+
+		// Get sorted set names
+		setNames := maputil.Keys(slos)
+		// For each set, count SLOs by label value
+		for i, setName := range setNames {
+			if i == 0 { // add zero columns for 0 count reporting
+				if labelValues, ok := labelDefs[labelName]; ok {
+					for _, labelValue := range labelValues {
+						hsetsLabelSetValue.Add(labelName, setName, labelValue, 0, true)
+					}
+				}
+			}
+			slos := slos[setName]
+			// Count SLOs for each label value
+			for _, slo := range slos {
+				if labels, exists := slo.Metadata.Labels[labelName]; exists {
+					for _, labelValue := range labels {
+						hsetsLabelSetValue.Add(labelName, setName, labelValue, 1, true)
+					}
+				}
+			}
+		}
+	}
+
+	hsetsLabelSetValue.UpdateSetOrders(labelDefs)
+
+	var sb strings.Builder
+	err := hsetsLabelSetValue.Markdown(&sb, "## ", "set name", true, &histogram.SetTablePivotOpts{
+		ColTotalRight:  true,
+		RowTotalBottom: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(w, sb.String())
+
+	return err
+}
