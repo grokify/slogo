ce409c1413757b11aa3957ae9563b143309b4797
John Wang <johncwang@gmail.com>
2025-10-20 04:24:01 -0700

    test: `examples/ai-agents`: add `TestReadExamples()`

--- examples/ai-agents/slos_test.go
+++ examples/ai-agents/slos_test.go
@@ -1,7 +1,10 @@
 package aiagents
 
 import (
+	_ "embed"
 	"testing"
+
+	"github.com/grokify/slogo"
 )
 
 func TestAIAgentsSLOs(t *testing.T) {
@@ -10,6 +13,33 @@ func TestAIAgentsSLOs(t *testing.T) {
 	for _, tt := range sloTests {
 		if err := tt.Validate(); err != nil {
 			t.Errorf("slo.Validate() error for %s: (%s)", tt.Metadata.Name, err.Error())
+		}
+	}
+}
+
+//go:embed agent-availability.json
+var agentAvailabilityJSON []byte
+
+//go:embed agent-availability.yaml
+var agentAvailabilityYAML []byte
+
+func TestReadExamples(t *testing.T) {
+	var readExamplesTests = []struct {
+		v        []byte
+		filename string
+		count    int
+	}{
+		{agentAvailabilityJSON, "agent-availability.json", 2},
+		{agentAvailabilityYAML, "agent-availability.yaml", 2},
+	}
+
+	for _, tt := range readExamplesTests {
+		objs, err := slogo.ReadFile(tt.filename)
+		if err != nil {
+			t.Errorf("slogo.Read(\"%s\") error [%s]", tt.filename, err.Error())
+		}
+		if len(objs) != tt.count {
+			t.Errorf("slogo.Read(\"%s\") mismatch on count want [%d] got [%d]", tt.filename, tt.count, len(objs))
 		}
 	}
 }
