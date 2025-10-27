e82f2f177107bd1be99612349522b2fb8c4b85ba
John Wang <johncwang@gmail.com>
2025-10-27 03:14:12 -0700

    enahnce: `requirement`: add YAML tags

--- requirement/requirement.go
+++ requirement/requirement.go
@@ -20,25 +20,25 @@ const (
 )
 
 type Requirement struct {
-	Name             string              `json:"name"`
-	DisplayName      string              `json:"displayName"`
-	Description      string              `json:"description"`
-	Goal             float64             `json:"goal"`
-	Comparison       cmputil.Operator    `json:"comparison"` // GTE, LTE, GT, LT
-	TimeTarget       duration.Spec       `json:"timeTarget"`
-	EvaluationWindow duration.Spec       `json:"evaluationWindow"`
-	Severity         Severity            `json:"severity"`
-	Owner            string              `json:"owner"`
-	Action           ActionSpec          `json:"action"`
-	Labels           map[string][]string `json:"labels"`
+	Name             string              `json:"name" yaml:"name"`
+	DisplayName      string              `json:"displayName" yaml:"displayName"`
+	Description      string              `json:"description" yaml:"description"`
+	Goal             float64             `json:"goal" yaml:"goal"`
+	Comparison       cmputil.Operator    `json:"comparison" yaml:"comparison"` // GTE, LTE, GT, LT
+	TimeTarget       duration.Spec       `json:"timeTarget" yaml:"timeTarget"`
+	EvaluationWindow duration.Spec       `json:"evaluationWindow" yaml:"evaluationWindow"`
+	Severity         Severity            `json:"severity" yaml:"severity"`
+	Owner            string              `json:"owner" yaml:"owner"`
+	Action           ActionSpec          `json:"action" yaml:"action"`
+	Labels           map[string][]string `json:"labels" yaml:"labels"`
 }
 
 // ActionSpec describes the target action or process for the SLORequirement.
 type ActionSpec struct {
-	Type        string   `json:"type"`        // e.g. "HTTP", "SQL", "Business", "Job"
-	Method      string   `json:"method"`      // e.g. "GET", "POST", "SELECT", "INSERT"
-	Target      string   `json:"target"`      // e.g. "/api/v1/users" or "SELECT * FROM users"
-	Service     string   `json:"service"`     // optional: logical service name
-	Environment string   `json:"environment"` // optional: "prod", "staging"
-	Tags        []string `json:"tags"`        // optional: arbitrary labels
+	Type        string   `json:"type" yaml:"type"`               // e.g. "HTTP", "SQL", "Business", "Job"
+	Method      string   `json:"method" yaml:"method"`           // e.g. "GET", "POST", "SELECT", "INSERT"
+	Target      string   `json:"target" yaml:"target"`           // e.g. "/api/v1/users" or "SELECT * FROM users"
+	Service     string   `json:"service" yaml:"service"`         // optional: logical service name
+	Environment string   `json:"environment" yaml:"environment"` // optional: "prod", "staging"
+	Tags        []string `json:"tags" yaml:"tags"`               // optional: arbitrary labels
 }
