3d91273ac1bb75703b0e447e0b9954e5cc952188
John Wang <johncwang@gmail.com>
2025-10-27 02:57:34 -0700

    enhance: `requirement`: add `Requirement.Labels`

--- requirement/requirement.go
+++ requirement/requirement.go
@@ -1,23 +1,36 @@
 package requirement
 
-import "github.com/grokify/mogo/time/duration"
+import (
+	"github.com/grokify/mogo/cmp/cmputil"
+	"github.com/grokify/mogo/time/duration"
+)
+
+type Severity string
 
 const (
 	ActionTypeHTTP = "HTTP"
 	ActionTypeSQL  = "SQL"
+
+	SeverityCritical      Severity = "critical"
+	SeverityHigh          Severity = "high"
+	SeverityMedium        Severity = "medium"
+	SeverityLow           Severity = "low"
+	SeverityInformational Severity = "informational"
+	SeverityUnknown       Severity = "unknown"
 )
 
 type Requirement struct {
-	Name             string        `json:"name"`
-	DisplayName      string        `json:"displayName"`
-	Goal             float64       `json:"goal"`
-	Comparison       string        `json:"comparison"` // GTE, LTE, GT, LT
-	TimeTarget       duration.Spec `json:"timeTarget"`
-	EvaluationWindow duration.Spec `json:"evaluationWindow"`
-	Severity         string        `json:"severity"`
-	Owner            string        `json:"owner"`
-	Description      string        `json:"description"`
-	Action           ActionSpec    `json:"action"`
+	Name             string              `json:"name"`
+	DisplayName      string              `json:"displayName"`
+	Description      string              `json:"description"`
+	Goal             float64             `json:"goal"`
+	Comparison       cmputil.Operator    `json:"comparison"` // GTE, LTE, GT, LT
+	TimeTarget       duration.Spec       `json:"timeTarget"`
+	EvaluationWindow duration.Spec       `json:"evaluationWindow"`
+	Severity         Severity            `json:"severity"`
+	Owner            string              `json:"owner"`
+	Action           ActionSpec          `json:"action"`
+	Labels           map[string][]string `json:"labels"`
 }
 
 // ActionSpec describes the target action or process for the SLORequirement.
